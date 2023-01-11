01876437097e3e36f83f564302ae84f7d1d5e003
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2023-01-11 17:05:56 +0100

    copy carlo

--- src/anmelden.php
+++ src/anmelden.php
@@ -1,7 +1,12 @@
-<html>
-    <title>
-        Anmelden
-    </title>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <link rel="stylesheet" href="../stylesheet.css">
+        <title>
+          Anmelden
+        </title>
+    </head>
+    
     <body>
         zurück zur: 
         <a href="../index.php">Startseite</a>
@@ -9,9 +14,55 @@
             <input type="text" size="20" name="user"/>
             <input type="password" size="20" name="pass"/>
             <input type="submit" value="Anmelden" name="okbutton"/>
-        </form>      
+        </form> 
+        
+        
+        <form method="post"> <!--Eingabefelder -->
+            <label>Vorname</label>
+            <input type="text" name="vorname" value="<?php echo $vorname; ?>">
+            <br>
+            <label>Nachname</label>
+            <input type="text" name="nachname" value="<?php echo $nachname; ?>">
+            <br>
+            <label>E-Mail</label>
+            <input type="text" name="email" value="<?php echo $email; ?>">
+            <br>
+            <label>Passwort</label>
+            <input type="text" name="passwort" value="<?php echo $passwort; ?>">
+            <br>
+            <label>Ort</label>
+            <input type="text" name="ort" value="<?php echo $ort; ?>">
+            <br>
+            <label>PLZ</label>
+            <input type="text" name="plz" value="<?php echo $plz; ?>">
+            <br>
+            <label>Straße</label>
+            <input type="text" name="straße" value="<?php echo $straße; ?>">
+            <br>
+            <label>Straßennummer</label>
+            <input type="text" name="straßennummer" value="<?php echo $straßennummer; ?>">
+            <br>
+
+            <?php
+                if(!empty($succesMessage)){ //meldung wenn es geklappt hat
+                 echo"
+                    <strong> $succesMessage </strong>
+                    <button type ='button' data-bs-dismiss='alert' aria-label='Close'></button>
+                 ";
+                }
+            ?>
+
+            <button type="submit">Abschicken</button>
+            <a href="../index.php" role="button">Abbrechen</a>
+        </form>
     </body>
-    <?php
+    <p>
+    <img src="../bILDER_SRC/bepett.png" width ="30%" height="30%"> <br>
+    
+    <h1>Bei uns sind sie sicher</h1>
+</html>
+
+<?php
         if (isset($_POST['okbutton'])) {
             echo "<h3>";
             echo $_POST['user'];
@@ -19,9 +70,71 @@
             echo "<h3>Wir freuen uns, Dich zu sehen!</h3>";
             echo "Dein sehr sicheres Passwort ist: ".$_POST['pass'];
         }
-    ?>
-    <p>
-    <img src="../bILDER_SRC/bepett.png" width ="50%" height="30%"> <br>
-    
-    <h1>Bei uns sind sie sicher</h1>
-</html>
+
+        //Stammvariabeln für DB
+        $servername = "127.0.0.1";
+        $username = "simon";
+        $password = "himbeerkuchen";
+        $db = "q2_shop";
+
+        //Verbindung aufbauen
+        $connection = new mysqli($servername, $username, $password, $db);
+
+
+        $vorname = "";
+    $nachname = "";
+    $email = "";
+    $passwort = "";
+    $ort = "";
+    $plz = "";
+    $straße = "";
+    $straßennummer = "";
+
+    $errorMessage = "";
+    $succesMessage = "";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){ //checken ob daten übertragen wurden
+        $vorname = $_POST["vorname"]; //wenn es klappt daten übertragen 
+        $nachname = $_POST["nachname"];
+        $email = $_POST["email"];
+        $passwort = $_POST["passwort"];
+        $ort = $_POST["ort"];
+        $plz = $_POST["plz"];
+        $straße = $_POST["straße"];
+        $straßennummer = $_POST["straßennummer"];
+
+        do{ 
+            if (empty($vorname) || empty($nachname) || empty($email) || empty($passwort) || empty($ort) || empty($plz) || empty($straße) || empty($straßennummer)){
+                $errorMessage = "Alle Felde müssen ausgefüllt sein";
+                break;
+            } //wenn ein feld leer ist error message
+
+            //einen kunden in die datenbank eintragen
+            $sql = "INSERT INTO kunden (vorname, nachname, email, passwort, ort, plz, straße, straßennummer) " .
+                    "VALUES ('$vorname', '$vorname', '$email', '$passwort', '$ort', '$plz', '$straße', '$straßennummer')";
+
+            $result = $connection->query($sql); //query ausführen
+            
+            if(!$result){ //ggf. fehler anzeigen
+                $errorMessage = "Invalid query: " . $connection->error;
+                break;
+            }
+
+            $vorname = "";
+            $nachname = "";
+            $email = "";
+            $passwort = "";
+            $ort = "";
+            $plz = "";
+            $straße = "";
+            $straßennummer = "";
+
+            $succesMessage = "Kunde wurde hinzugefügt";
+
+            header("location: /tools/admin.php"); //wenn es funktioniert hat den user zur seite zurückschicken
+            exit;
+
+        } while (false);
+    }
+
+?>
