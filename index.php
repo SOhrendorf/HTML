c42fefcc21244767d1020f497395366d8978a20e
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2023-01-11 16:23:07 +0100

    new structure

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="stylesheet.css">
+        <title>
+            Seepferdchen Shop
+        </title>
+    </head>
+
+    <body>
+        <img src="img/walkurelogo.png" alt="logo">
+        <table>
+            <tr>
+                <th>
+                        <a href="index.php">Startseite</a>
+                </th>
+                <th>
+                        <a href="src/impressum.html">Impressum</a>
+                </th>
+                <th>
+                    <a href="src/anmelden.php">Anmelden</a>
+                </th>    
+            </tr>
+        </table>
+    </body>
+</html>
